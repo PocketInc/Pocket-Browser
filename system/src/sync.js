7bfdbfbfc4c851c043f29f3a8834fca7b1117de1
AhmadHrb <ahmad@pocket-inc.ml>
2020-12-18 16:50:55 +0200

    Fully working Sync

--- system/src/sync.js
+++ system/src/sync.js
@@ -1,9 +1,12 @@
 const fetch = require("node-fetch");
-
+const fs = require("fs");
+let dataPath = require("electron").remote.app.getPath("userData");
+let data;
+let opts;
 async function getSync() {
     if (localStorage.getItem('login')) {
         let login = JSON.parse(localStorage.getItem('login'));
-    const opts = {
+    opts = {
         headers: {
             cookie: 'PHPSESSID=' + login.token
         }
@@ -11,8 +14,8 @@ async function getSync() {
 let state = await fetch("https://pocket-inc.ml/api/browser/logged.php?redirect=browser",opts).then(response => response.json());
 
 if (state.state === "success") {
-    let data = await fetch("https://pocket-inc.ml/api/browser/get_data.php",opts).then(response => response.json());
-    document.getElementById("sync").innerHTML = "<small>Logged in: <b>" + state.user + "</b></small><br><small>Latest Sync: <b>" + data.latest + "</b></small><br><br><button>Sync</button> <button>Load</button>";
+    data = await fetch("https://pocket-inc.ml/api/browser/get_data.php",opts).then(response => response.json());
+    document.getElementById("sync").innerHTML = "<small>Logged in: <b>" + state.user + "</b></small><br><small>Latest Sync: <b>" + data.latest + "</b></small><br><br><button onclick='Sync()'>Sync</button> <button onclick='loadSync()'>Load</button>";
 } else if(state.state === "error") {
         document.getElementById("sync").innerHTML = "<p>Email:<br><input id='em' type='email'></p><p>Password:<br><input id='pas' type='password'></p><button onclick='login()'>Login</button>"
 }
@@ -42,4 +45,24 @@ async function login() {
         document.getElementById("info").innerHTML = "Invaild Account!"
         document.getElementById("info").style.color = "red";
     }
+}
+function loadSync() {
+if (data.data['home']) fs.writeFileSync(dataPath + "/data/home.pocket",data.data['home']);
+if (data.data['search']) fs.writeFileSync(dataPath + "/data/engine.pocket",data.data['search'])
+if (data.data['adb']) fs.writeFileSync(dataPath + "/data/adb.pocket",data.data['adb']);
+if (data.data['theme']) fs.writeFileSync(dataPath + "/data/theme.pocket",data.data['theme'])
+if (data.data['website']) fs.writeFileSync(dataPath + "/data/website.pocket",data.data['website'])
+    console.log("Done!")
+    document.getElementById("info").innerHTML = "Done!";
+    document.getElementById("info").style.color = "black";
+}
+function Sync() {
+if (fs.readFileSync(dataPath + "/data/home.pocket")) fetch("https://pocket-inc.ml/api/browser/save_data.php?type=home&data=" + fs.readFileSync(dataPath + "/data/home.pocket",{encoding: "utf8"}), opts)
+    if (fs.readFileSync(dataPath + "/data/engine.pocket")) fetch("https://pocket-inc.ml/api/browser/save_data.php?type=search&data=" + fs.readFileSync(dataPath + "/data/engine.pocket",{encoding: "utf8"}),opts)
+    if (fs.readFileSync(dataPath + "/data/adb.pocket")) fetch("https://pocket-inc.ml/api/browser/save_data.php?type=adb&data=" + fs.readFileSync(dataPath + "/data/adb.pocket",{encoding: "utf8"}),opts)
+    if (fs.readFileSync(dataPath + "/data/theme.pocket")) fetch("https://pocket-inc.ml/api/browser/save_data.php?type=theme&data=" + fs.readFileSync(dataPath + "/data/theme.pocket",{encoding: "utf8"}),opts)
+    if (fs.readFileSync(dataPath + "/data/website.pocket")) fetch("https://pocket-inc.ml/api/browser/save_data.php?type=website&data=" + fs.readFileSync(dataPath + "/data/website.pocket",{encoding: "utf8"}),opts)
+
+
+
 }
